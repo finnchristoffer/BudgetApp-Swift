3e7e7ef5f38f3660b6853b07c4c6b4f286f579f6
finnchristoffer <finn.christoffer@gmail.com>
2023-01-27 17:14:28 +0700

    first commit

---
+++ BugdetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BugdetApp
+//
+//  Created by Finn Christoffer Kurniawan on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
